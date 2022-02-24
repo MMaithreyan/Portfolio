c671f96cc4de668b935fce454b0c683cbcdbb231
MMaithreyan <maithreyanm8@gmail.com>
2022-02-25 00:22:59 +0530

    Calculator

--- run.js
+++ run.js
@@ -1,11 +1,13 @@
 function load() {
     document.getElementById("time").style.display = "none";
     document.getElementById("port").style.display = "block";
+}
+setTimeout(load, 100);
 
+//function myFunction1() {
+  //  var element = document.body;
+    //element.classList.toggle("dark-mode");
+//}
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
   }
-  setTimeout(load, 000);
-  
-  function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
