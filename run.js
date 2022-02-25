985a0a89581e5ea0e057527cc4310ee8e27be5cd
MMaithreyan <maithreyanm8@gmail.com>
2022-02-25 22:18:06 +0530

    Calculator

--- run.js
+++ run.js
@@ -2,12 +2,11 @@ function load() {
     document.getElementById("time").style.display = "none";
     document.getElementById("port").style.display = "block";
 }
-setTimeout(load, 100);
+setTimeout(load, 1000);
 
-//function myFunction1() {
-  //  var element = document.body;
-    //element.classList.toggle("dark-mode");
-//}
-function myFunction(x) {
-    x.classList.toggle("fa-thumbs-down");
-  }
+function myFunction1() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    document.getElementsByClassName("nav-link").style.color = "white";
+}
